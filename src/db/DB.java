4f5fc553acd1e251168cb119270c9a5faa4d2cc9
Syahrul <syahkill375@gmail.com>
2026-06-25 19:24:23 +0800

    Update database connection settings in DB.java for improved clarity and maintainability

--- src/db/DB.java
+++ src/db/DB.java
@@ -8,20 +8,21 @@ public class DB {
 
     // Change these based on your XAMPP settings
     private static final String URL =
-            "jdbc:mysql://localhost:3306/habitap?useSSL=false&serverTimezone=UTC";
-    private static final String USER = "root";
-    private static final String PASS = ""; // XAMPP default is empty
+            "jdbc:mysql://localhost:3306/habitap?useSSL=false&serverTimezone=UTC"; // jdbc:mysql://localhost:3306/s72430_habitap
+    private static final String DRIVERNAME = "com.mysql.cj.jdbc.Driver";
+    private static final String USERNAME = "root"; // s72430
+    private static final String PASSWORD = ""; // aKwjyLcvhPfB
 
     static {
         try {
             // MySQL Connector/J 8
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName(DRIVERNAME);
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("MySQL JDBC Driver not found. Add connector JAR to WEB-INF/lib.", e);
         }
     }
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASS);
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
 }
